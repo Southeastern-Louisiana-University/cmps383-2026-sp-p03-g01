1287fea3d3624a2eab13e2721aa9fafbdeb9f79d
Jonathan Soileau <soileau_jonathan@yahoo.com>
2026-03-23 22:17:53 -0500

    Clean up + Locations Page
    Cleaned up some example files from the lecture repo and added an almost working locations page

--- Selu383.SP26.Web/src/Router.tsx
+++ Selu383.SP26.Web/src/Router.tsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router";
-import Home from "@/pages/Home.tsx";
-import { TestPage } from "@/pages/TestPage";
+
 import { NavBar } from "@/components/NavBar";
-//import { LocationsPage } from "@/pages/LocationsPage";
+
+import Home from "@/pages/Home";
+import LocationsPage from "@/pages/LocationsPage";
 
 export function Router() {
   return (
     <Routes>
       <Route element={<NavBar />}>
         <Route index element={<Home />} />
-        <Route path="test" element={<TestPage />} />
-        <Route path="locations" element={<div>Locations List Page</div>} />
+        <Route path="locations" element={<LocationsPage />} />
       </Route>
     </Routes>
   );
--- Selu383.SP26.Web/src/context/UserLoggedInContext.tsx
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const UserLoggedInContext = React.createContext(false);
