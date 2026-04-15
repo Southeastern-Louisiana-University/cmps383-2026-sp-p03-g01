d59ce7423fa0d1247091892a6667444a5fb3215e
alex-mackeral <alexander.campagne@selu.edu>
2026-04-15 10:43:23 -0500

    ImagesAndBag
    Redoing this

---
+++ Selu383.SP26.Api/Migrations/20260415153759_AddEditBagsAndImages.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP26.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEditBagsAndImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BagItems_Item_ItemId",
+                table: "BagItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExtraOption_Item_ItemId",
+                table: "ExtraOption");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Item",
+                table: "Item");
+
+            migrationBuilder.RenameTable(
+                name: "Item",
+                newName: "Items");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BagItems_Items_ItemId",
+                table: "BagItems",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExtraOption_Items_ItemId",
+                table: "ExtraOption",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BagItems_Items_ItemId",
+                table: "BagItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExtraOption_Items_ItemId",
+                table: "ExtraOption");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.RenameTable(
+                name: "Items",
+                newName: "Item");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Item",
+                table: "Item",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BagItems_Item_ItemId",
+                table: "BagItems",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExtraOption_Item_ItemId",
+                table: "ExtraOption",
+                column: "ItemId",
+                principalTable: "Item",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
